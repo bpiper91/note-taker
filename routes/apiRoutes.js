1a9c81e51331096a3bd830f0674226e23a4e17f5
bpiper91 <bpiper91@gmail.com>
2022-08-11 23:15:11 -0400

    added server.js and routes directory, added HTML routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,4 @@
+// GET /api/notes -> read db.json and return all saved notes as json
+// POST /api/notes -> receive new note to save in request body, add to db.json, return new note to client (with unique ID)
+// DELETE /api/notes/:id -> receive query param of id, read db.json, filter by !id, rewrite db.json
+
---
+++ server.js
@@ -0,0 +1,24 @@
+// load express
+const express = require('express');
+
+// set port
+const PORT = process.env.PORT || 3001;
+
+// set express instance to variable
+const app = express();
+
+// load routes
+//const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes.js');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// indicate which route to use on URLs
+//app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
