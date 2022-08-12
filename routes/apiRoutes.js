0739973622af7745bf86b6fb8e496619ac2c8f1f
bpiper91 <bpiper91@gmail.com>
2022-08-12 10:38:46 -0400

    deployed app to heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,19 @@
+// load express and router
 const router = require('express').Router();
+// load file system for read/write functions
 const fs = require('fs');
+// link db.json for saved notes
 const db = require('./../db/db.json');
+// load uuid for unique IDs
 const {v4: uuidv4} = require('uuid');
 
+// get existing notes
 router.get('/notes', (req, res) => {
     // read db.json to get the saved notes
     res.json(db);
 });
 
+// add a note
 router.post('/notes', (req, res) => {
     // receive new note from request body
     let newNote = req.body;
@@ -29,6 +35,7 @@ router.post('/notes', (req, res) => {
     });
 });
 
+// delete a note
 // router.delete('notes/:id', (req, res) => {
 //     // receive query param of id
 //     // read saved notes
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,19 @@
+// load path for responding with HTML pages
 const path = require('path');
+// load express and router
 const router = require('express').Router();
 
+// send home page back on / request
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// send notes page back on /notes request
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
+// send home page back on any other request
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
